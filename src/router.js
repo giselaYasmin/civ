02bc0cec738177378665e7b21b07109f9cd4bf45
Gisela Espinoza <gespinoza.ugb@gmail.com>
2023-02-21 11:00:17 -0600

    Update home route

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ export default createRouter({
     history: createWebHistory(),
     routes: [
         {
-            path: '/',
+            path: '/civ',
             name: 'Home',
             component: Home
         },
